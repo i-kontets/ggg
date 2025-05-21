ef5747c5a5f6975fd4d5cdfefcabec8a9b5f1065
i-kontets <2410019@i-seifu.jp>
2025-05-21 14:31:28 +0900

    push

--- ono_test/simulation/mypage.php
+++ ono_test/simulation/mypage.php
@@ -40,26 +40,6 @@
             </table>
         </div>
 </div>
-<script>
-    //以下headerの高さを取得してmainの開始位置を指定
-    // DOMが完全に読み込まれてから実行
-    document.addEventListener('DOMContentLoaded', function() {
-        const header = document.getElementById('myHeader');
-        const main = document.getElementById('myMain');
-
-        // ヘッダーの高さを取得
-        const headerHeight = header.offsetHeight;
-        console.log(headerHeight);
-        
-        // mainタグのpadding-topに設定
-        main.style.paddingTop = headerHeight + 'px';
-
-        // ウィンドウのリサイズ時にも高さを再調整する
-        window.addEventListener('resize', function() {
-            const updatedHeaderHeight = header.offsetHeight;
-            main.style.paddingTop = updatedHeaderHeight + 'px';
-        });
-    });
-</script>
+<script src="js/standard.js"></script>
 </body>
 </html>
